b01c98cb1d317b5e36e98e86d6e04ef2ca3bb4fc
Disidd <dmitriy.sidorenko97@gmail.com>
2024-08-29 12:06:18 +0200

    HM17

--- HM17/index.js
+++ HM17/index.js
@@ -1,35 +1,31 @@
 cards = [];
 suits = ['clubs', 'spades', 'diamonds', 'hearts'];
-values = ['2', '3', '4', '5', '6', '7', '8', '9', '10',];
-cardImg = ['jack','queen','king','t']
+values = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'J','Q','K','T'];
 
 for (i = 0; i < values.length; i++) {
     for (j = 0; j < suits.length; j++) {
-        // console.log(values[i],suits[j]);
-            cards.push(`<div class="card">
-                <div class='card_info>${values[i]}<img src="images/${suits[j]}.svg" alt="${suits[j]}"></div>
-                <div class='card_info>${values[i]}<img src="images/${suits[j]}.svg" alt="${suits[j]}"></div>
+        if (!isNaN(values[i])){
+            cardInfo = (`<div class="card">
+                <div class='card_info'>${values[i]}
+                    <img src="images/${suits[j]}.svg" alt="${suits[j]}">
+                </div>
+                <div class='card_info'>${values[i]}
+                    <img src="images/${suits[j]}.svg" alt="${suits[j]}">
+                </div>
+            </div>`);   
+        } else {cardImg = (values[i] === 'J') ? 'J' : (values[i] === 'Q') ? 'Q' : (values[i] === 'K') ? 'K' : suits[j];
+            cardInfo = (`<div class="card">
+                <div class='card_info'>${values[i]}
+                    <img src="images/${suits[j]}.svg" alt="${suits[j]}">
+                </div>
+                <img class="card--person" src="images/${cardImg}.svg" alt="${cardImg}">
+                <div class='card_info'>${values[i]}
+                    <img src="images/${suits[j]}.svg" alt="${suits[j]}">
+                </div>
             </div>`);
-    }
-}
-
-for (i = 0; i < cardImg.length; i++) {
-    for (j = 0; j < suits.length; j++) {
-            cards.push(`<div class="card card--person">
-                <div class='card_info>${cardImg[i]}<img src="images/${suits[j]}.svg" alt="${suits[j]}"></div>
-                <div class='card_info>${cardImg[i]}<img src="images/${suits[j]}.svg" alt="${suits[j]}"></div>
-                <img class="person" src="images/${cardImg[i]}.svg" alt="${cardImg[i]}">
-            </div>`);
-            if (cardImg === 'T') {
-                cards.push(`<div class="card card--person">
-                    <img class="person" src="images/${suits[i]}.svg" alt="${suits[i]}">
-                </div>`);
         }
-
+            cards.push(cardInfo);   
     }
 }
-
-document.write (`<div class='wrapper'>${cards.join('')}</div>`)
-
-
+document.write(`<div class='wrapper'>${cards.join('')}</div>`);
 
